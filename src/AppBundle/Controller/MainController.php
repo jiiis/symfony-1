746c04ed11304e88c9987d61e15ac51e7dbf951b
jiiis <yngwie.chang@gmail.com>
2015-05-30 21:04:37 +1000

    Replaced "render()" method call with Sensio FrameworkExtraBundle Template annotation.

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -3,15 +3,17 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MainController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Template()
      */
     public function indexAction()
     {
-        return $this->render('AppBundle:main:index.html.twig');
+        return [];
     }
 }
