4e21deab4a6c2f8c6f52e4c58e4fc4ec456fecf9
jiiis <yngwie.chang@gmail.com>
2015-05-30 21:17:15 +1000

    Tried "render" and "controller" functions in Twig templates.

---
+++ src/AppBundle/Controller/ContentController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ContentController extends Controller
+{
+    /**
+     * @Template()
+     */
+    public function bottomAction()
+    {
+        return [];
+    }
+}
