82d6777dd892d898f6e5f03842f7da9ec24b5242
Hao <yngwie.chang@gmail.com>
2015-07-25 16:19:34 +1000

    Tried session in both controllers and Twig templates.

--- src/App/Controller/ContentController.php
+++ src/App/Controller/ContentController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class ContentController extends Controller
 {
@@ -13,5 +15,17 @@ class ContentController extends Controller
     public function bottomAction()
     {
         return [];
+    }
+
+    /**
+     * @Route("test-session/{session_name}/{session_value}", name="test_session")
+     * @Template()
+     */
+    public function testSessionAction(Request $request, $session_name, $session_value)
+    {
+        $session = $request->getSession();
+        $session->set($session_name, $session_value);
+
+        return compact('session_name', 'session_value');
     }
 }
