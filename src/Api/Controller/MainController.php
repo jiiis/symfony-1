c30f9a899b51a6c4ef1ddae2576249b11c38cc02
Hao <yngwie.chang@gmail.com>
2015-07-25 14:44:57 +1000

    Tested "Request".

--- src/Api/Controller/MainController.php
+++ src/Api/Controller/MainController.php
@@ -3,7 +3,9 @@
 namespace Api\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/api")
@@ -21,5 +23,24 @@ class MainController extends Controller
     public function pangcaicaiAction($_format)
     {
         return $this->render('Api:main:pangcaicai.' . $_format . '.twig');
+    }
+
+    /**
+     * @Route(
+     *      "/test-request.{_format}",
+     *      name="test_request",
+     *      defaults={"_format"="json"},
+     *      requirements={"_format"="json"}
+     * )
+     * @Template()
+     */
+    public function testRequestAction(Request $request)
+    {
+        return [
+            'info' => [
+                'is_xhr' => $request->isXmlHttpRequest(),
+                'query_p1' => $request->query->get('p1')
+            ]
+        ];
     }
 }
